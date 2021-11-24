8c658d432cfaf61e3609d998af6463a80358289b
Lyappunov <lyappunov19@outlook.com>
2021-11-24 03:47:24 -0800

    console.log

--- routes/record.js
+++ routes/record.js
@@ -76,6 +76,7 @@ const upload = multer({ storage: storage }).single('file')
 
 // This section will help you get a list of all the records.
 recordRoutes.route("/record").get(function (req, res) {
+  console.log(dbo.getDb())
   let db_connect = dbo.getDb();
   db_connect
     .collection("records")
